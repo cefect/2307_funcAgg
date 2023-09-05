0b5c5bd9fde676141de609070918fb333e59383b
cefect <cefect@gmail.com>
2023-09-05 10:43:36 +0200

    Update _01_jgrid.py
    descritize again

--- agg/_01_jgrid.py
+++ agg/_01_jgrid.py
@@ -8,6 +8,7 @@ create aggregation grids
 
 import os, hashlib, sys, subprocess, psutil
 from datetime import datetime
+from itertools import product
 
 import psycopg2
 print('psycopg2.__version__=' + psycopg2.__version__)
@@ -82,10 +83,104 @@ def build_extents_grid(conn_d, epsg_id, schema, tableName):
     return  
 
 
-def build_agg_grid(grid_size_l, country_l, conn_d, schema, tableBaseName, tableName2, epsg_id, log):
+
+def _build_agg_grid_country_size(grid_size, country_key, tableName, conn_d, schema,tableName2, epsg_id, log):
+    """build a grid table for the country +grid_size combination"""
+    
+    #===========================================================================
+    # setup and build
+    #===========================================================================
+    start_i = datetime.now()
+    with psycopg2.connect(get_conn_str(conn_d)) as conn:
+        #===========================================================================
+        # setup
+        #===========================================================================
+        #remove if it exists
+        with conn.cursor() as cur:
+            cur.execute(f"""DROP TABLE IF EXISTS {schema}.{tableName}""")
+        conn.commit()
+        
+        #=======================================================================
+        # build
+        #=======================================================================
+        with conn.cursor() as cur:
+            #===================================================================
+            # create table
+            #===================================================================
+            cmd_str = f"""
+            CREATE TABLE {schema}.{tableName} AS
+                SELECT  %s as country_key, {grid_size} as grid_size, (ST_SquareGrid({grid_size}, geom)).*
+                    FROM {schema}.{tableName2} a
+                        WHERE a.country_key=%s
+                        """
+            log.info(cmd_str)
+            cur.execute(cmd_str, (country_key,country_key,))
+            
+            #===================================================================
+            # #delete irrellevant grids
+            #===================================================================
+            cmd_str = f"""
+            DELETE FROM {schema}.{tableName} a
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM grids.country_grids b
+                    WHERE ST_Intersects(a.geom, ST_Transform(b.geometry, {epsg_id}))
+                )"""
+            log.info(cmd_str)
+            cur.execute(cmd_str)
+            
+            #===================================================================
+            # post
+            #===================================================================
+            #register the geometry for QGIS
+            with conn.cursor() as cur:
+                cur.execute(f"""SELECT Populate_Geometry_Columns(%s::regclass)""", (f'{schema}.{tableName}', ))
+            #change dtype on grid_size
+            with conn.cursor() as cur:
+                cur.execute(f"""
+                ALTER TABLE {schema}.{tableName}
+                    ALTER COLUMN grid_size TYPE integer;
+                    """)
+                
+            #create spatial index
+            with conn.cursor() as cur:
+                cur.execute(f"""
+                    CREATE INDEX {tableName}_geom_idx
+                        ON {schema}.{tableName}
+                            USING GIST (geom);
+                    """)
+                
+    #===========================================================================
+    # wrap
+    #===========================================================================
+    with psycopg2.connect(get_conn_str(conn_d)) as conn:
+        #query result
+        with conn.cursor() as cur:
+            cur.execute(f"""
+                SELECT country_key, COUNT(*)
+                    FROM {schema}.{tableName}
+                        GROUP BY country_key;
+                """)
+            #return series as resuilt
+            res = pd.Series(dict(cur.fetchall()))
+            assert len(res) > 0
+            log.info(f'built grid={grid_size} w/\n%s' % res)
+ 
+    #meta
+    meta_d = {
+        'tdelta':(datetime.now() - start_i).total_seconds(), 
+        'RAM_GB':psutil.virtual_memory()[3] / 1000000000, 
+        'postgres_GB':get_directory_size(postgres_dir)}
+        #'output_MB':os.path.getsize(ofp)/(1024**2)
+    log.info(f'finished {grid_size}\n    {dstr(meta_d)}\n\n')
+    
+    return res
+
+def build_agg_grids(grid_size_l, country_l, conn_d, schema, tableBaseName, tableName2, epsg_id, log):
     """buidl aggregated grids per country
     
     decided to create 1 table per grid-size as these will be large operations
+        still too slow. grouping by country as well
     """
  
     #===========================================================================
@@ -94,94 +189,12 @@ def build_agg_grid(grid_size_l, country_l, conn_d, schema, tableBaseName, tableN
     log.info(f'looping on {len(grid_size_l)} grids')
     res_d = dict()
  
-    for i, grid_size in enumerate([int(e) for e in grid_size_l]):
-        tableName=f'{tableBaseName}_{grid_size:07d}'
-        log.info(f'on {i}: {tableName}')
-        start_i =datetime.now()
-        
-        with psycopg2.connect(get_conn_str(conn_d)) as conn: 
-            #===========================================================================
-            # setup
-            #===========================================================================
  
-            #remove if it exists
-            with conn.cursor() as cur:
-                cur.execute(f"""DROP TABLE IF EXISTS {schema}.{tableName}""")            
-            conn.commit()
-            
-            #create table
-            with conn.cursor() as cur:          
-                cmd_str=f"""
-                    CREATE TABLE {schema}.{tableName} AS
-                        SELECT a.country_key, {grid_size} as grid_size, (ST_SquareGrid({grid_size}, geom)).* 
-                            FROM {schema}.{tableName2} a
-                                """
-                log.info(cmd_str) 
-                cur.execute(cmd_str)
-                
-                #delete irrellevant grids
-                cmd_str=f"""
-                    DELETE FROM {schema}.{tableName} a
-                        WHERE NOT EXISTS (
-                            SELECT 1
-                            FROM grids.country_grids b
-                            WHERE ST_Intersects(a.geom, ST_Transform(b.geometry, {epsg_id}))
-                        )"""
-                         
-                log.info(cmd_str) 
-                cur.execute(cmd_str)
-                
-                 
-                #register the geometry for QGIS
-                with conn.cursor() as cur:
-                    cur.execute(f"""SELECT Populate_Geometry_Columns(%s::regclass)""", (f'{schema}.{tableName}',))
-                    
-                #change dtype on grid_size
-                with conn.cursor() as cur:
-                    cur.execute(f"""
-                    ALTER TABLE {schema}.{tableName}
-                        ALTER COLUMN grid_size TYPE integer;
-                            """)  
-                    
-                    
-                #create spatial index
-                with conn.cursor() as cur:
-                    cur.execute(f"""
-                    CREATE INDEX {tableName}_geom_idx
-                        ON {schema}.{tableName}
-                            USING GIST (geom);
-                            """)  
-                
-            
- 
-                
- 
-        with psycopg2.connect(get_conn_str(conn_d)) as conn: 
-            #query result
-            with conn.cursor() as cur:
-                cur.execute(f"""
-                SELECT country_key, COUNT(*)
-                        FROM {schema}.{tableName}
-                            GROUP BY country_key;
-                        """)
-                
-                #return series as resuilt
-                res = pd.Series(dict(cur.fetchall()))
-                assert len(res)>0
-                log.info(f'built grid={grid_size} w/\n%s'%res)
-                res_d[grid_size] = res
-                
-        #=======================================================================
-        # wrap grid
-        #=======================================================================
-        meta_d = {
-                'tdelta':(datetime.now()-start_i).total_seconds(),
-                'RAM_GB':psutil.virtual_memory () [3]/1000000000,
-                'postgres_GB':get_directory_size(postgres_dir),
-                #'output_MB':os.path.getsize(ofp)/(1024**2)
-                }
-
-        log.info(f'finished {grid_size}\n    {dstr(meta_d)}\n\n')
+    #iterate over all combinations
+    for i, (grid_size, country_key) in enumerate(product([int(e) for e in grid_size_l], country_l)):
+        tableName=f'{tableBaseName}_{country_key}_{grid_size:07d}'
+        log.info(f'on {i}: {tableName}')            
+        _build_agg_grid_country_size(grid_size, country_key, tableName, conn_d, schema,tableName2, epsg_id, log)
                 
  
                 
@@ -195,15 +208,18 @@ def build_agg_grid(grid_size_l, country_l, conn_d, schema, tableBaseName, tableN
   
 
 def run_join_agg_grids(
-        country_l=None,
+ 
         out_dir=None,
         conn_d=postgres_d,
         epsg_id=None,
         schema='grids',
         grid_size_l=[
-            30*34,30*8, 30*2
-            #1e5, 2e5, #big for testing
-            ]
+            #30*34,30*8, 30*2
+            1e5, 
+            #2e5, #big for testing
+            ],
+        
+        country_l = ['bgd'],
         ):
     #===========================================================================
     # defaults
@@ -239,7 +255,7 @@ def run_join_agg_grids(
     log.info(f'buidling {tbl_agg} w/ \n    {grid_size_l}')
  
     #grid_size_l, country_l, conn_d, schema, tableName, tableName2, epsg_id
-    _ = build_agg_grid(grid_size_l, country_l, conn_d, schema, tbl_agg,  tbl_extents, epsg_id, log)
+    _ = build_agg_grids(grid_size_l, country_l, conn_d, schema, tbl_agg,  tbl_extents, epsg_id, log)
     
     #===========================================================================
     # wrap
