00b035378a7f8c2650eb4ef029155898238194ec
cefect <cefect@gmail.com>
2023-09-09 08:17:22 +0200

    plot_gradient_matrix

--- funcMetrics/da_funcs.py
+++ funcMetrics/da_funcs.py
@@ -107,7 +107,9 @@ print('loaded matplotlib %s'%matplotlib.__version__)
 #===============================================================================
 import os, math
 import pandas as pd
+idx = pd.IndexSlice
 import numpy as np
+
 
 from datetime import datetime
 
@@ -126,6 +128,30 @@ def transform_1d_to_2d(arr, cols, constant_values=-9999):
     return np.resize(padded_arr, (rows, cols))
 
 
+
+def _get_matrix_plot_map_df(model_l, figsize, ncols):
+    """take a list and reshape it onto 2D for setting up a matrix plot"""
+    #build a dataframe to map the model_id onto the grid
+    model_df = pd.DataFrame(transform_1d_to_2d(np.array(model_l), ncols))
+    model_df.columns = [f'c{e}' for e in model_df.columns]
+    model_df.index = [f'r{e}' for e in model_df.index]
+#get figsize (holding the width)
+    if figsize is None:
+        figsize_width = matplotlib.rcParams['figure.figsize'][0]
+        figsize_height = figsize_width * (model_df.shape[0] / model_df.shape[1])
+        figsize = figsize_width, figsize_height
+#setup the figure with this
+    col_keys, row_keys = model_df.columns.tolist(), model_df.index.tolist()
+    return row_keys, col_keys, figsize, model_df
+
+
+def _update_fancy_names(model_names_d, serx):
+    for model_id in serx.index.unique('model_id'):
+        if not model_id in model_names_d:
+            model_names_d[model_id] = serx.xs(model_id, level='model_id').index.unique('abbreviation')[0].replace(' et al.', '.')
+    
+    return 
+
 def plot_dfunc_matrix(
         fp=None,
         out_dir=None,
@@ -141,7 +167,7 @@ def plot_dfunc_matrix(
     #===========================================================================
     if fp is  None: fp = dfunc_pkl_fp
     if out_dir is None:
-        out_dir=os.path.join(wrk_dir, 'outs', 'da', 'exposure', today_str)
+        out_dir=os.path.join(wrk_dir, 'outs', 'funcs', 'da', today_str)
     if not os.path.exists(out_dir):os.makedirs(out_dir)
     
     log = init_log(fp=os.path.join(out_dir, today_str+'.log'), name='dfunc')
@@ -158,9 +184,7 @@ def plot_dfunc_matrix(
         
  
     #update the names
-    for model_id in serx.index.unique('model_id'):
-        if not model_id in model_names_d:
-            model_names_d[model_id] = serx.xs(model_id, level='model_id').index.unique('abbreviation')[0].replace(' et al.', '.')
+    _update_fancy_names(model_names_d, serx)
  
         
     
@@ -176,20 +200,8 @@ def plot_dfunc_matrix(
     #===========================================================================
     # setup figure
     #===========================================================================
-    #build a dataframe to map the model_id onto the grid
-    model_df = pd.DataFrame(transform_1d_to_2d(np.array(model_l), ncols))
-    model_df.columns = [f'c{e}' for e in model_df.columns]
-    model_df.index = [f'r{e}' for e in model_df.index]    
+    row_keys, col_keys, figsize, model_df = _get_matrix_plot_map_df(model_l, figsize, ncols)
     
-    #get figsize (holding the width)
-    if figsize is None:
-        figsize_width=matplotlib.rcParams['figure.figsize'][0]
-        figsize_height = figsize_width*(model_df.shape[0]/model_df.shape[1])
-        
-        figsize = (figsize_width, figsize_height)
- 
-    #setup the figure with this
-    col_keys, row_keys  = model_df.columns.tolist(), model_df.index.tolist()
     fig, ax_d = get_matrix_fig(row_keys, col_keys, log=log, set_ax_title=False, 
                                sharex=True, sharey=True, figsize=figsize, add_subfigLabel=False)
  
@@ -286,12 +298,193 @@ def plot_dfunc_matrix(
     return ofp
     
     
-     
+
+def plot_gradient_matrix(
+        fp=None,
+        out_dir=None,
+        figsize=None,
+        dfid_l=[49, 798, 811, 449, 481, 402],
+        #dfid_l=None,
+ 
+        model_names_d=clean_names_d.copy(),
+        line_kwargs_d = {
+            'rl':dict(linestyle='solid', marker='o', fillstyle='none'),
+            'deriv1':dict(linestyle='dashed', marker='v'),
+            'deriv2':dict(linestyle='dotted', marker='^')
+            }
+        ):
+    """plot function, gradient1, and gradient2
+    only using eaxmple functions
+    """
+    
+    #===========================================================================
+    # defaults
+    #===========================================================================
+ 
+    if out_dir is None:
+        out_dir=os.path.join(wrk_dir, 'outs', 'funcs', 'da', today_str)
+    if not os.path.exists(out_dir):os.makedirs(out_dir)
+    
+    log = init_log(fp=os.path.join(out_dir, today_str+'.log'), name='grad')
+    
+ 
+    #===========================================================================
+    # load data from first
+    #===========================================================================
+    
+    dx_raw = pd.read_pickle(fp)#.xs('residential', level='sector_attribute')
+    
+    if dfid_l is None:
+        #dfid_l = dx_raw.index.unique('df_id').tolist()
+        
+ 
+        
+        #all non-flemo
+        tvals = tuple(set(dx_raw.index.unique('model_id')).difference([3]))
+        
+        bx = np.isin(dx_raw.index.get_level_values('model_id'), tvals)
+        
+        dfid_l = dx_raw.loc[bx, :].index.unique('df_id').tolist()[40:60]
+        #=======================================================================
+        # print()
+        # 
+        # dx_raw.loc[bx, :].groupby('df_id').plot()
+        #=======================================================================
+ 
+        
+    #slice
+    dx = dx_raw.loc[np.isin(dx_raw.index.get_level_values('df_id'),dfid_l), :]
+ 
+    #update the names
+    _update_fancy_names(model_names_d, dx)
+ 
+        
+    
+    """
+    dxind.index.unique('model_id')
+    """
+    log.info(f' loaded {dx.shape}')
+    
+    dfid_cnt = len(dx.index.unique('df_id'))
+    log.info(f'plotting {dfid_cnt} functions')
+    
+ 
+    #===========================================================================
+    # setup figure
+    #===========================================================================
+    row_keys = ['']
+    col_keys = dfid_l
+ 
+    if figsize is None:
+        #figsize_width = matplotlib.rcParams['figure.figsize'][0]
+        figsize_width = dfid_cnt*3
+        figsize_height = figsize_width / dfid_cnt
+        
+        figsize = figsize_width, figsize_height
+    
+    fig, ax_d = get_matrix_fig(row_keys, col_keys, log=log, set_ax_title=False, 
+                               sharex=True, sharey=True, figsize=figsize, add_subfigLabel=False)
+ 
+ 
+    #uncessary levels
+    drop_lvl_names = list(set(dx.index.names).difference(['df_id', 'wd']))
+    #===========================================================================
+    # plot loop    
+    #===========================================================================
+    cnt=0
+    rc_ax_iter = [(row_key, col_key, ax) for row_key, ax_di in ax_d.items() for col_key, ax in ax_di.items()]
+    for row_key, col_key, ax in rc_ax_iter:
+ 
+        #load meta        
+        mdex = dx.xs(col_key, level='df_id').index #get index data        
+        model_id = mdex.unique('model_id')[0] 
+        fancy_name = model_names_d[model_id]
+        
+        
+        #get data
+        df = dx.xs(col_key, level='df_id').droplevel(drop_lvl_names) 
+        
+        #plot each
+        for name, col in df.items():
+            col.plot(ax=ax, 
+                     #color='black', 
+                     **line_kwargs_d[name])
+ 
+ 
+ 
+ 
+        """
+        plt.show()
+        view(mdf)
+        """
+        
+        #add text
+        #mdf = serx.xs(model_id, level='model_id').droplevel(['df_id', 'wd']).index.to_frame().reset_index(drop=True).drop_duplicates(keep='first').dropna(axis=0, how='all')
+        
+ 
+ 
+        tstr = f'%s\n'%fancy_name+f'model={model_id}\n'+f'func={col_key}'
+            
+            #f'sector=%s\n'%mdf['sector_attribute'][0]
+            #f'real_frac={bx.sum()/len(bx):.4f}'
+            
+        ax.text(0.95, 0.05, tstr, 
+                            transform=ax.transAxes, va='bottom', ha='right', 
+                            bbox=dict(boxstyle="round,pad=0.3", fc="white", lw=0.0,alpha=0.5 ),
+                            )
+        
+        cnt+=1
+        
+ 
+ 
+    log.info(f'plot built w/ {cnt:,.0f} records')
+    #===========================================================================
+    # post
+    #===========================================================================
+    for row_key, col_key, ax in rc_ax_iter:
+        
+        #set limits
+        ax.set_ylim(-100,100)
+        ax.set_xlim(0,10.0)
+        ax.grid()
+            
+        #first row
+        if row_key==row_keys[0]:
+            if col_key==col_keys[-1]: #last col
+                ax.legend()
+            
+        #last row
+        if row_key==row_keys[-1]:
+            ax.get_xaxis().set_major_formatter(
+                matplotlib.ticker.FuncFormatter(lambda x, p: '%i'%(x*100)))
+            ax.set_xlabel('depth (cm)')
+            
+        #first col
+        if col_key==col_keys[0]:
+            ax.set_ylabel('relative loss (pct)')
+            
+
+            
+
+    
+    #===========================================================================
+    # write
+    #===========================================================================
+    ofp = os.path.join(out_dir, f'dfuncs_gradient_{len(col_keys)}x{len(row_keys)}.svg')
+    fig.savefig(ofp, dpi = 300,   transparent=True)
+    log.info(f'wrote to \n    %s'%ofp)
+    
+    return ofp
+    
+    
+   
 if __name__=='__main__':
     
 
 
-    plot_dfunc_matrix()
+    #plot_dfunc_matrix()
+    
+    plot_gradient_matrix(fp=r'l:\10_IO\2307_funcAgg\outs\funcs\01_deriv\derivs_3266_20230907.pkl')
 
     
  
