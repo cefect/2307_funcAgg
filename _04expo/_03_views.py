6df7daa9ea8848c561d60d9ebec12fb98c2cf9d6
cefect <cefect@gmail.com>
2023-09-24 13:23:11 +0200

    move create_view_join_stats_to_rl

--- _04expo/_03_views.py
+++ _04expo/_03_views.py
@@ -132,120 +132,20 @@ def create_view_merge_stats(country_key, haz_key,
     return tableName
 
 
-def create_view_join_stats_to_rl(
-        country_key, haz_key,
- 
-        log=None,
-        conn_str=None,
-        dev=False,
- 
- 
-        with_geom=False,
-        ):
-    """merge rl, rl_mean, wd views (run_view_join_depths()) with expo stats (create_view_merge_stats())"""
-        
-
-    #if grid_size_l is None: grid_size_l = gridsize_default_l
-    
-    #===========================================================================
-    # defaults
-    #===========================================================================
-    
-    start = datetime.now()   
-    
-    country_key = country_key.lower() 
- 
-    if conn_str is None: conn_str = get_conn_str(postgres_d)
-    if log is None:
-        log = init_log(name=f'jstats')
-        
-    sql = lambda x:pg_exe(x, conn_str=conn_str, log=log)
-    
-    #===========================================================================
-    # talbe params
-    #===========================================================================
- 
-    #grids w/ grid RL, mean RL, grid WD ... see run_view_join_depths()
-    table_left=f'rl_mean_grid_{country_key}_{haz_key}_wd'
-    
-    #grids w/ bldg_cnt and wet_cnt
-    table_right=f'grid_bldg_stats_{country_key}_{haz_key}' #wd for all grids. see _02agg._07_views()
-    
-    tableName = f'grid_rl_wd_bstats_{country_key}_{haz_key}'
- 
-    
-    if dev:
-        schema='dev'
-        schema_left, schema_right=schema, schema        
-
-    else:
-        schema='damage'  #makes more sense here I think
-        schema_left='damage'
-        schema_right='expo'       
-        
-    
-    keys_l = ['country_key', 'grid_size', 'i', 'j']
- 
-    log.info(f'creating view from {table_left} and {table_right}')
-    #===========================================================================
-    # join depths
-    #===========================================================================
-    sql(f'DROP MATERIALIZED VIEW IF EXISTS {schema}.{tableName}')    
-    
-    
-    cmd_str = f'CREATE MATERIALIZED VIEW {schema}.{tableName} AS \n'
-    
-    link_cols = ' AND '.join([f'tleft.{e}=tright.{e}' for e in keys_l]) 
-    cmd_str+= f"""
-        SELECT tleft.*, tright.bldg_cnt, tright.wet_cnt
-            FROM {schema_left}.{table_left} AS tleft
-                LEFT JOIN {schema_right}.{table_right} AS tright
-                    ON {link_cols}
-            """
-    
-    sql(cmd_str)
-    
-    #===========================================================================
-    # join geometry
-    #===========================================================================
-    if with_geom:
-        create_view_join_grid_geom(schema, tableName, country_key, log=log, dev=dev, conn_str=conn_str)
-    
-    
-    
-    
-    #===========================================================================
-    # wrap
-    #===========================================================================
-        #meta
-    meta_d = {
-        'tdelta':(datetime.now() - start).total_seconds(), 
-        'RAM_GB':psutil.virtual_memory()[3] / 1000000000, 
-        #'postgres_GB':get_directory_size(postgres_dir)}
-        #'output_MB':os.path.getsize(ofp)/(1024**2)
-        }
-    log.info(f'finished on {tableName} w/ \n{meta_d}')
-    
-    return tableName
-
-
 
 
 
 def run_all(ck='deu', e='f500_fluvial', **kwargs):
-    log = init_log(name=f'grid_rl')
+    log = init_log(name=f'expo.views')
     
     create_view_merge_stats(ck, e, log=log, **kwargs)
     
-    create_view_join_stats_to_rl(ck, e, log=log, **kwargs)
-    
-    from _03damage._05_mean_bins import get_grid_rl_dx
-    get_grid_rl_dx(ck, e, log=log, use_cache=False, **kwargs)
+ 
     
 
 if __name__ == '__main__':
-    pass
-    #create_view_merge_stats('deu', 'f500_fluvial', dev=False)
+ 
+    #create_view_merge_stats('deu', 'f500_fluvial', dev=True)
     
     #create_view_join_stats_to_rl('deu', 'f500_fluvial', dev=False, with_geom=False)
     
